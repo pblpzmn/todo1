86245dab9394bd1301c284ae6808648c16d30c75
Pablo Pazmino <ppazmino@todo1.com>
2018-05-02 19:31:45 -0500

    starting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Person {
+  name: string;
+  age: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  person: Person = {
+    name: 'Pablo Pazmiño',
+    age : 33,
+  };
+  /*constructor(name, age) {
+    this.person.name = name;
+    this.person.age = age;
+  }*/
+  name: 'Pablo';
 }
